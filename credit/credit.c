139ff93b6ae124e8b5b3f76e2aab4782babe08b8
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-21 10:39:20 +0000

    Sat, Jan 21, 2023, 10:39 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,6 +5,12 @@ int main(void)
 {
   // Set Variable n to be the actual credit card number
   long n;
+  int j;
+  float y = (float) n;
+   for (j=(-1);y>1;j++)
+  {
+    y = (y/10);
+  }
 
     do
     {
@@ -12,14 +18,9 @@ int main(void)
     }
     while((j<13) && (j==15) && (j>16));
 
-  int j;
-  float y = (float) n;
-  for (j=0;y>1;j++)
-  {
-    y = (y/10);
-  }
 
-  j = j+1;
+
+
 
 
   // Set Variable M to be the length of the credit card number

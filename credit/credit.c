7a4ccb5f41ad6ea80fba6a7de2dd8b84b159d619
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-21 10:53:02 +0000

    Sat, Jan 21, 2023, 10:53 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,6 +3,9 @@
 
 int main(void)
 {
+
+  //this whole section is validating whether the input is actually valid. ()
+
   // Set Variable n to be the actual credit card number
   long n;
 
@@ -12,7 +15,7 @@ int main(void)
 
     float y = (float) n;
 
-    //calculating the number of digits in the credit card number
+    //calculating the number of digits in the credit card number (denoted by j)
 
     for (j=(-1);y>1;j++)
     {
@@ -26,18 +29,22 @@ int main(void)
     {
         n = get_long("Credit Card Number Please. ");
 
+        y = n;
+
+
         j=(-1);
 
         for (j=(-1);y>1;j++)
-     {
-        y = (y/10);
-     }
+        {
+            y = (y/10);
+        }
 
     }
     while((j<13) || (j==14) || (j>16));
 
-
     }
+
+
 
 
 

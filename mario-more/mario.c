d6468ee86e5496217ce9d273929fb51f9575cf40
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-21 04:46:34 +0000

    Sat, Jan 21, 2023, 4:46 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -43,7 +43,7 @@ int main(void)
             printf("#");
             i++;
         }
-        while ( ( ( (y-l) -1) < i) && ( i < (y-1)) )
+        while ( ( ( (y-l) -1) < i) && ( i < y) )
         {
             printf(" ");
             i++;

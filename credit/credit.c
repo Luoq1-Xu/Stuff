9ec6ab57d242548411ed48aab36057276a9e69a3
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-21 10:32:36 +0000

    Sat, Jan 21, 2023, 10:32 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,11 +6,22 @@ int main(void)
   // Set Variable n to be the actual credit card number
   long n;
 
-  //Get a variable for the numberof digits of the credit card number (n)
-
-  do
-  {
     n = get_long("Credit Card Number Please. ");
+
+    float y = (float) n;
+
+  // Set Variable M to be the length of the credit card number
+  int j;
+
+  for (j=0;y>1;j++)
+  {
+    y = (y/10);
   }
-  while ((n<1) && (n<13) && (n==14) && (n>16));
+
+  j = j+1;
+
+  //j is the number of digits.
+
+
+
 }

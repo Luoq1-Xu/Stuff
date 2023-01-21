a4f83d1ff7deb7517afa5e93fabca8d81aec2e76
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-21 06:03:07 +0000

    Sat, Jan 21, 2023, 6:03 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+  long n;
+
+  do
+  {
+    n = get_long("Credit Card Number Please. ");
+  }
+  while (n<1 || n>);
 }

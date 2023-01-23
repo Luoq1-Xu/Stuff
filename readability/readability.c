24a19a7b272f30109c78784399bcfc282a113f30
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-23 04:39:11 +0000

    Mon, Jan 23, 2023, 4:39 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+   string text = get_string("Text: \n");
+}
 
+
+
+
+int count_letters(string text)
+{
+    for (int i=0;text[i] != '\0'; i++)
+    {
+        if (((text[i] >= 65) && (text[i] <= )))
+
+    }
 }

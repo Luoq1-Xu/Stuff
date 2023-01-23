acbc6d322c27a3cc06b3f5c9b881e817bd9685b5
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-23 13:50:47 +0000

    Mon, Jan 23, 2023, 1:50 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,12 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 
 int main(int argc, string argv[])
 {
-   string plaintext = get_string("plaintext: \n");
-   
+
+    int length = strlen (argv[1]);
+
+    if (argc != 2)
+    {
+         printf("Usage: ./substitution key\n");
+        return 1;
+    }
+    else if ( length != 26)
+    {
+         printf("Key must contain 26 characters.\n");
+        return 1;
+    }
+
+    string plaintext = get_string("plaintext: \n");
 
 }
 
@@ -14,7 +28,4 @@ int main(int argc, string argv[])
 
 
 //Program to read the input
-string convert_cipher(string plaintext)
-{
 
-}

ececac8a44a54b28dbf5394fa8fa6198f6f2a6dc
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-23 15:01:54 +0000

    Mon, Jan 23, 2023, 3:01 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -53,12 +53,12 @@ int main(int argc, string argv[])
                 if (isupper(p[l]))
                 {
                       p[l] = tolower(p[l]);
-                      p[l] = x[(p[l]-64)];
+                      p[l] = x[(p[l]-96)];
                       p[l] = toupper(p[l]);
                 }
                 else if (islower(p[l]))
                 {
-                    p[l] = x[(p[l]-64)];
+                    p[l] = x[(p[l]-96)];
                 }
                 printf("after %i\n",p[l]);
             }

cd639ad22f867df77e9b08cf08556cff5a58889c
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-23 03:25:43 +0000

    Mon, Jan 23, 2023, 3:25 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -26,9 +26,9 @@ int compute_score(string word)
     // TODO: Compute and return score for string
     for (int i=0, j=strlen(word1); i<strlen(word1);i++)
     {
-        if ( ((word1[i]>=65) && (word1[i])<=90) || ((word[i]>=97) && (word[i]<=122)) )
+        if ( ((word[i]>=65) && (word[i])<=90) || ((word[i]>=97) && (word[i]<=122)) )
         {
-            
+             
         }
 
     }

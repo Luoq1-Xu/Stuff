d773092be2b6c002febfc2bb780bebd838a5f567
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-23 04:57:09 +0000

    Mon, Jan 23, 2023, 4:57 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,13 +3,20 @@
 
 int count_letters(string text);
 
+int count_words(string text);
+
 int main(void)
 {
    string text = get_string("Text: ");
 
    int letters = count_letters(text);
 
+   int words = count_words(text);
+
    printf("%i letters\n",letters);
+
+   printf("%i words \n", words);
+
 }
 
 
@@ -36,3 +43,17 @@ int count_letters(string text)
 }
 
 
+int count_words(string text)
+{
+    int words = 0;
+
+    for (int i=0;text[i] != '\0'; i++)
+    {
+        if (text[i]==32)
+        {
+            words ++;
+        }
+    }
+    return words;
+
+}

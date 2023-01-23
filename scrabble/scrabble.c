001acc47d995066e54f6c66343fc9db7a52a8a37
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-23 03:37:27 +0000

    Mon, Jan 23, 2023, 3:37 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -24,14 +24,15 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
-    int score;
-    for (int i=0, j=strlen(word1); i<strlen(word1);i++)
+    int score=0;
+    for (int i=0, j=strlen(word); i<strlen(word);i++)
     {
         if ( ((word[i]>=65) && (word[i])<=90) || ((word[i]>=97) && (word[i]<=122)) )
         {
              toupper(word[i]);
-             score += POINTS[]
+             score += POINTS[(word[i]-65)];
         }
-
+        printf("%i\n",score);
     }
+    return score;
 }

cb02aab8f40452a2991f2601bf8a35486f4d8550
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-25 13:45:45 +0000

    Wed, Jan 25, 2023, 9:45 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -120,7 +120,10 @@ void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if(strcmp (candidate))
+        }
     }
     // TODO
     return;

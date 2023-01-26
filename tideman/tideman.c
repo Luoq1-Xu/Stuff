ac05eeadbc2ad8f5460b606a857e4ab97c19f142
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-26 05:25:30 +0000

    Thu, Jan 26, 2023, 1:25 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -154,7 +154,10 @@ void sort_pairs(void)
     // TODO
     for (int i = 0; i < pair_count; i++)
     {
-        
+        for (int j = 0; j < pair_count; j++)
+        {
+            
+        }
     }
 
     return;

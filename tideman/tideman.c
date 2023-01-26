91b2eba42972a2d1652e905e611bb5ed41be456c
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-26 13:46:30 +0000

    Thu, Jan 26, 2023, 9:46 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -139,8 +139,8 @@ void add_pairs(void)
         {
             if (preferences[i][j] > preferences[j][i])
             {
-                pairs[j].winner = i;
-                pairs[j].loser = j;
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
                 pair_count++;
             }
         }

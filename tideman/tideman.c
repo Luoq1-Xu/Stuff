6cf4a007cc8a56516a1a189ac97a3d2e91fae6f8
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-26 15:00:09 +0000

    Thu, Jan 26, 2023, 11:00 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -267,12 +267,13 @@ void print_winner(void)
 
 int checklosers(int currentwinner, int currentloser)
 {
-    int x = currentwinner
+    int x = currentloser
     for (int j = 0; j < candidate_count; j++)
         {
             if (locked[x][j] == true;)
             {
-                x = j
+                x = j;
+
             }
         }
 }

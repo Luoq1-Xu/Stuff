eb87e45b3ca320b6454b240e27eb56cf17b7a3b9
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-30 13:06:37 +0000

    Mon, Jan 30, 2023, 9:06 PM +08:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -3,5 +3,12 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
-    
+    for (int i = 0, n = height; i < n; i++)
+    {
+        for (int j = 0, m = width; j < m; j++)
+        {
+            if (RGBTRIPLE [i][j].rgbtBlue = 0 && )
+        }
+    }
+
 }

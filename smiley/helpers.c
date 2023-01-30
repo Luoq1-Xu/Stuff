59c6f8b509f3de7ab161e001a1798c7b2c2f8d0d
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-30 15:20:19 +0000

    Mon, Jan 30, 2023, 11:20 PM +08:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -3,12 +3,16 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
+    // Cycle through rows
     for (int i = 0, n = height; i < n; i++)
     {
+        // Cycling through each pixel in each row
         for (int j = 0, m = width; j < m; j++)
         {
+            // If pixel is black (all values of rgb will be zero)
             if (image[i][j].rgbtBlue == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtRed == 0x00)
             {
+                //Change each pixel rgb values
                 image[i][j].rgbtBlue = 0xff;
                 image[i][j].rgbtGreen = 0x00;
                 image[i][j].rgbtRed = 0x00;

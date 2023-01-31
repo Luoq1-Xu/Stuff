75223d28b88f0974e18ef44aa7873119ce618e73
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-01-31 12:44:31 +0000

    Tue, Jan 31, 2023, 8:44 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -24,7 +24,16 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int temp;
+            
+
+        }
+    }
+
     return;
 }
 

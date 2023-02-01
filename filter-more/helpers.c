b29325824c4f6d2a1cc0030fd937ffee79b8740c
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-01 14:41:56 +0000

    Wed, Feb 1, 2023, 10:41 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -41,9 +41,9 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             tempgreen = image[i][j].rgbtGreen;
             tempred = image[i][j].rgbtRed;
 
-            image[i][j].rgbtBlue = image[i][width-j].rgbtBlue;
-            image[i][j].rgbtGreen = image[i][width-j].rgbtGreen;
-            image[i][j].rgbtRed = image[i][width-j].rgbtRed;
+            image[i][j].rgbtBlue = image[i][width-j-1].rgbtBlue;
+            image[i][j].rgbtGreen = image[i][width-j-1].rgbtGreen;
+            image[i][j].rgbtRed = image[i][width-j-1].rgbtRed;
 
             image[i][width-j].rgbtBlue = tempblue;
             image[i][width-j].rgbtGreen = tempgreen;

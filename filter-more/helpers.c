e49d48e946930379433641b3b90b58252c3315b4
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-01 05:29:45 +0000

    Wed, Feb 1, 2023, 1:29 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -125,7 +125,7 @@ void cornerpixel(int a, int b, int c, int d, int e)
 {
     int tempblue = round ((image[a][b].rgbtBlue + image[a][c].rgbtBlue + image[d][b].rgbtBlue + image[d][e].rgbtBlue)/4.0);
     newpixel[0][0].rgbtBlue = tempblue;
-    int tempgreen = round ((image[a][b].rgbtGreen + image[a][c].rgbtGreen + image[d][b].rgbtGreen + image[e][e].rgbtGreen)/4.0);
+    int tempgreen = round ((image[a][b].rgbtGreen + image[a][c].rgbtGreen + image[d][b].rgbtGreen + image[d][e].rgbtGreen)/4.0);
     newpixel[0][0].rgbtGreen = tempgreen;
     int tempred = round ((image[a][b].rgbtRed + image[a][c].rgbtRed + image[d][b].rgbtRed + image[e][f].rgbtRed)/4.0);
     newpixel[0][0].rgbtRed = tempred;

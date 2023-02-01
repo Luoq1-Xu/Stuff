67ca09da1510db6851729fafd00c11692f00c8bd
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-01 12:19:00 +0000

    Wed, Feb 1, 2023, 8:19 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -84,7 +84,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             {
                 int a = i;
                 int b = j;
-                int c 
+                int c = 
 
             }
             //First column but not corners

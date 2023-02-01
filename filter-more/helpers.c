98483ba781f339317c5632f50e9cce7f79384308
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-01 05:22:33 +0000

    Wed, Feb 1, 2023, 1:22 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -125,14 +125,14 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 
 
 //BLUR CORNER PIXEL
-int cornerpixel(int a, int b, int c, int d)
+void cornerpixel(int a, int b, int c, int d)
 {
-    int tempblue = round ((image[a][a].rgbtBlue + image[a][b].rgbtBlue + image[b][a].rgbtBlue + image[][1].rgbtBlue)/4.0);
+    int tempblue = round ((image[a][b].rgbtBlue + image[a][c].rgbtBlue + image[d][b].rgbtBlue + image[][1].rgbtBlue)/4.0);
     newpixel[0][0].rgbtBlue = tempblue;
     int tempgreen = round ((image[0][0].rgbtGreen + image[0][1].rgbtGreen + image[1][0].rgbtGreen + image[1][1].rgbtGreen)/4.0);
     newpixel[0][0].rgbtGreen = tempgreen;
     int tempred = round ((image[0][0].rgbtRed + image[0][1].rgbtRed + image[1][0].rgbtRed + image[1][1].rgbtRed)/4.0);
     newpixel[0][0].rgbtRed = tempred;
 
-
+    return;
 }

4271b6de3ae66bcfdeee0d6354a0d65649cee0d4
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-03 12:57:51 +0000

    Fri, Feb 3, 2023, 8:57 PM +08:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -11,7 +11,11 @@ int main(int argc, char *argv[])
 {
     // Ensure proper usage
     // TODO #1
-    if (argc != 2)
+    if (argc != 3)
+    {
+        printf("Usage: ./reverse input.wav output.wav\n");
+        return 1;
+    }
 
     // Open input file for reading
     // TODO #2

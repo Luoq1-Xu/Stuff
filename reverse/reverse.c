a7c38e0701d5b64edcaf4254f042b68986c907b5
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-04 03:52:15 +0000

    Sat, Feb 4, 2023, 11:52 AM +08:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -36,8 +36,8 @@ int main(int argc, char *argv[])
 
     // Read header into an array
     // TODO #3
-    WAVHEADER wavhead;
-    fread(&wavhead, sizeof(WAVHEADER), 1, inptr);
+    WAVHEADER header;
+    fread(&header, sizeof(WAVHEADER), 1, inptr);
 
     // Use check_format to ensure WAV format
     // TODO #4

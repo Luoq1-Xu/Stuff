48482ebf2abc4b9472c02a43174a2502e80fdb4a
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-04 04:05:20 +0000

    Sat, Feb 4, 2023, 12:05 PM +08:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -58,8 +58,14 @@ int main(int argc, char *argv[])
 int check_format(WAVHEADER header)
 {
     // TODO #4
-    
-    return 0;
+    if(header.format[0] == 'W' && header.format[1] == 'A' && header.format[2] == 'V' && header.format[3] == 'E')
+    {
+        return 1;
+    }
+    else
+    {
+        printf()
+    }
 }
 
 int get_block_size(WAVHEADER header)

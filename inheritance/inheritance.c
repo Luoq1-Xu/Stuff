c7e6c44371fce2bb2698f3b17fd5432607d941ee
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-06 06:00:24 +0000

    Mon, Feb 6, 2023, 2:00 PM +08:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -40,8 +40,12 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    person p = malloc(sizeof(person));
-    if ()
+    person *p = malloc(sizeof(person));
+    if (p == NULL)
+    {
+        printf("Ran out of Memory!\n");
+        return 1;
+    }
     // If there are still generations left to create
     if (generations > 1)
     {
@@ -50,7 +54,7 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-
+        
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
 
     }

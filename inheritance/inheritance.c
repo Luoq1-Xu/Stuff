875c58ec92b45665dbb63a9347e4bc31789592b0
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-07 05:42:47 +0000

    Tue, Feb 7, 2023, 1:42 PM +08:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -58,7 +58,7 @@ person *create_family(int generations)
         p->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        p->allelles[0] = parent0->
+        int allelle1 = rand() % 1
 
 
     }

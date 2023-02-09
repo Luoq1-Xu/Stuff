eb0f3b7001d60f73ab1cf9137d1c83c744d5f87e
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-09 14:00:53 +0000

    Thu, Feb 9, 2023, 10:00 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -96,7 +96,7 @@ bool load(const char *dictionary)
             {
                 point = point->next;
             }
-            if (point->word[0])
+            if (point->word[0] == )
 
 
         }

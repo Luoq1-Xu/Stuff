5d103a7a2e5d647efed55a3a88016478a4f3327b
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-10 05:50:22 +0000

    Fri, Feb 10, 2023, 1:50 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -93,7 +93,7 @@ bool load(const char *dictionary)
 
             if (table[key]->word[0] < 97 || table[key]->word[0] > 122)
             {
-                for (int i =0; i < counter; i++)
+                for (int i = 0; i < counter; i++)
                 {
                     point->word[i] = tempword[i];
                 }
@@ -109,7 +109,7 @@ bool load(const char *dictionary)
                 temp->next = table[key];
                 table[key] = temp;
 
-                for (int i =0; i < counter; i++)
+                for (int i = 0; i < counter; i++)
                 {
                     temp->word[i] = tempword[i];
                 }

eeaf73f234d96968411c3fe74b511cdf6b46497a
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-15 12:38:33 +0000

    Wed, Feb 15, 2023, 8:38 PM +08:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,7 +16,7 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-    file = open("{argv[1]}","r")
+    file = open("{sys.argv[1]}","r")
     teams.append(csv.dictreader())
 
 

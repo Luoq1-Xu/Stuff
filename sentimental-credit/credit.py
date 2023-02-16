f13560a15f8d73d66b47477bfdf51e66b7d52200
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-16 14:44:53 +0000

    Thu, Feb 16, 2023, 10:44 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -14,10 +14,17 @@ if len(temp) != 13 and len(temp) != 15 and len(temp) != 16 :
     quit()
 
 product = 0
-for i in range(len(temp) - 1, 0, -2):
+for i in range(len(temp) - 2, 0, -2):
     r = temp[i] * 2
     product += r % 10
     product += trunc(r / 10)
+
+for i in range(len(temp) - 1, 0, -2):
+    product += temp[i]
+
+if (product % 10) == 0 and :
+
+
 
 
 

317c1b7a5f0082113211ba7fb86d35f30f8d1ad6
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-16 14:36:19 +0000

    Thu, Feb 16, 2023, 10:36 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -14,7 +14,9 @@ if len(temp) != 13 and len(temp) != 15 and len(temp) != 16 :
 
 product = 0
 for i in range(len(temp), 0, -2):
-    product += temp[i] * 2
+    r = temp[i] * 2
+    product += r % 10
     
+
 
 

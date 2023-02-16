b0d8604aaa43e63387563859e02be0964749505b
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-16 11:57:10 +0000

    Thu, Feb 16, 2023, 7:57 PM +08:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,11 +1,12 @@
 # TODO
 from cs50 import get_int
 
+# Get number (and keep trying again if number not valid)
 while True:
     rows = get_int("Height: ")
     if rows > 0 and rows < 9:
         break
-
+# print rows
 for counter in range(1, rows + 1):
     print((" " * (rows - counter)) + ("#" * counter) + "  " + ("#" * counter))
 

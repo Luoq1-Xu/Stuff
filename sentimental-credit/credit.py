f68d3bccea58f0e15e9500b15a1b474ec709e8a4
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-16 13:38:02 +0000

    Thu, Feb 16, 2023, 9:38 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,8 +1,11 @@
 # TODO
 
-from cs50 import get_int
+from cs50 import get_string
 
 input = get_string("Number: ")
 
 list = [input]
-if len(list) != 13, 15, 16
+if len(list) != (13, 15, 16):
+    print("INVALID\n")
+    quit()
+

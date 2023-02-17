a365ca0d45ad33458806c1c9d48d046ab350f42e
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-17 05:45:54 +0000

    Fri, Feb 17, 2023, 1:45 PM +08:00

--- dna/dna.py
+++ dna/dna.py
@@ -10,13 +10,16 @@ def main():
         sys.exit(1)
 
     # TODO: Read database file into a variable
+    database = []
     with open(sys.argv[1], 'r') as csvfile:
         reader = csv.dictreader(csvfile)
         for row in reader:
-            
+            database.append(row)
 
 
     # TODO: Read DNA sequence file into a variable
+    with open(sys.argv[2], 'r') as dna:
+        read
 
     # TODO: Find longest match of each STR in DNA sequence
 

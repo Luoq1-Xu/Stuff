194cf122d684a61c950674db548fcbe603c3f513
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-19 15:22:27 +0000

    Sun, Feb 19, 2023, 11:22 PM +08:00

---
+++ movies/12.sql
@@ -0,0 +1,6 @@
+SELECT title FROM movies WHERE id IN
+(SELECT movie_id FROM
+(SELECT movie_id, COUNT(*) AS times FROM
+(SELECT movie_id FROM stars WHERE person_id IN
+(SELECT id FROM people WHERE name = 'Johnny Depp' OR name = 'Helena Bonham Carter')) GROUP BY movie_id)
+WHERE times > 1);

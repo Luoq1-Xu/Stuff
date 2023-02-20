1f9bcfbe0f4a7755613d94c5b4ada53a6ff81c48
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-20 12:29:42 +0000

    Mon, Feb 20, 2023, 8:29 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,10 +1,14 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Finding the crime scene report relating to the theft of the duck
-SELECT description FROM crime_scene_reports WHERE description LIKE '%duck%';
+SELECT description
+  FROM crime_scene_reports
+ WHERE description
+  LIKE '%duck%';
 
 -- Gathering info on the pertinent crime scene report
-SELECT id,year,month,day,street FROM crime_scene_reports WHERE description IN (SELECT description FROM crime_scene_reports WHERE description LIKE '%duck%');
+SELECT id,
+       year,month,day,street FROM crime_scene_reports WHERE description IN (SELECT description FROM crime_scene_reports WHERE description LIKE '%duck%');
 
 -- Now we know the crime occured in 2021 , 7th month, 28 day on Humphrey Street
 

e4cde54e3ed729ccec2968b27af58b8745fc81d6
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-20 12:02:10 +0000

    Mon, Feb 20, 2023, 8:02 PM +08:00

--- movies/4.sql
+++ movies/4.sql
@@ -1,1 +1,7 @@
-SELECT count(*) FROM (SELECT title FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE rating = '10.0'));
+SELECT count(*)
+  FROM (SELECT title
+          FROM movies
+         WHERE id IN
+               (SELECT movie_id
+                  FROM ratings
+                 WHERE rating = '10.0'));

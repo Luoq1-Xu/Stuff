07ba6815db9531f95dcc933f2f96996a8ab17a6f
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-20 05:25:54 +0000

    Mon, Feb 20, 2023, 1:25 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -34,3 +34,10 @@ SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 A
 -- Names of the owners of the possible vehicles
 SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25 AND  activity = 'exit');
 
+
+-- Let's go back to Raymond's clue about the flight
+
+-- Earliest Possible Flight out of fiftyville on 29-7-2021
+SELECT id FROM flights WHERE origin_airport_id IN (SELECT id FROM airports WHERE city = 'Fiftyville') AND year = 2021 AND month = 7 AND day = 29 ORDER BY hour,minute LIMIT 1;
+
+-- Passengers on that flight

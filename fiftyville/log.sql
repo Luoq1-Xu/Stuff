f25b941cc0f17695a730cc3bbd9e530632ba1c68
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-20 12:35:43 +0000

    Mon, Feb 20, 2023, 8:35 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -32,20 +32,47 @@ SELECT name,
 -- Let's Follow Eugene's Lead
 
 -- Check for all transactions on that day
-SELECT account_number, amount , transaction_type FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw';
+SELECT account_number, amount , transaction_type
+  FROM atm_transactions
+ WHERE year = 2021
+   AND month = 7
+   AND day = 28
+   AND atm_location = 'Leggett Street'
+   AND transaction_type = 'withdraw';
 
 -- Get a list of possible suspects based on Eugene's lead
-SELECT name FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw'));
+SELECT name
+  FROM people
+ WHERE id IN
+       (SELECT person_id
+          FROM bank_accounts
+         WHERE account_number IN
+               (SELECT account_number
+                  FROM atm_transactions
+                 WHERE year = 2021
+                   AND month = 7
+                   AND day = 28
+                   AND atm_location = 'Leggett Street'
+                   AND transaction_type = 'withdraw'));
 
 -- Now let's try Raymond's Lead
 
 -- List of possible suspects based on Raymond's Lead
-SELECT name FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60 );
+SELECT name
+  FROM people
+ WHERE phone_number IN
+       (SELECT caller
+          FROM phone_calls
+         WHERE year = 2021
+           AND month = 7
+           AND day = 28
+           AND duration < 60 );
 
 -- Now let's try Ruth's Lead
 
 -- Possible license plates of cars that fit the bill
-SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25 AND  activity = 'exit';
+SELECT license_plate
+  FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25 AND  activity = 'exit';
 
 -- Names of the owners of the possible vehicles
 SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25 AND  activity = 'exit');

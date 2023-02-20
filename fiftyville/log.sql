0013ac626071a66cf19481c85ddb37af48d32cb0
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-20 12:31:03 +0000

    Mon, Feb 20, 2023, 8:31 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -8,12 +8,24 @@ SELECT description
 
 -- Gathering info on the pertinent crime scene report
 SELECT id,
-       year,month,day,street FROM crime_scene_reports WHERE description IN (SELECT description FROM crime_scene_reports WHERE description LIKE '%duck%');
+       year,month,day,
+       street
+  FROM crime_scene_reports
+ WHERE description IN
+       (SELECT description
+          FROM crime_scene_reports
+         WHERE description
+          LIKE '%duck%');
 
 -- Now we know the crime occured in 2021 , 7th month, 28 day on Humphrey Street
 
 -- Getting all interviews on the date of the theft
-SELECT name, transcript FROM interviews WHERE year =2021 AND month = 7 AND day = 28;
+SELECT name,
+       transcript
+  FROM interviews
+ WHERE year = 2021
+   AND month = 7
+   AND day = 28;
 
 -- Ruth, Eugene and Raymond provide useful information relating to the theft.
 

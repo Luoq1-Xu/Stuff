6974ce67a7e8e4d98a1eaaaa9b37fdc2c248e8cc
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-02-20 04:16:25 +0000

    Mon, Feb 20, 2023, 12:16 PM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Finding the crime scene report relating to the theft of the duck
+SELECT description FROM crime_scene_reports WHERE description LIKE '%duck%';
+

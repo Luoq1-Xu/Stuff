ceec239bd244c5781f194c97064c17f952435de5
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-06-09 13:24:42 +0000

    Fri, Jun 9, 2023, 9:24 PM +08:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -10,13 +10,11 @@ if argc < 3:
 elif argc > 3:
     sys.exit("Too many command-line arguments")
 
-print(((sys.argv[1].rsplit("."))[0]))
-
-if ((sys.argv[1].rsplit("."))[0] not in ["jpg", "jpeg", "png"]):
+if ((sys.argv[1].split("."))[1] not in ["jpg", "jpeg", "png"]):
     sys.exit("Invalid input")
-if ((sys.argv[2].rsplit("."))[0] not in ["jpg", "jpeg", "png"]):
+if ((sys.argv[2].split("."))[1] not in ["jpg", "jpeg", "png"]):
     sys.exit("Invalid output")
-if (sys.argv[1].rsplit("."))[0] != (sys.argv[2].rsplit("."))[0]:
+if (sys.argv[1].rsplit("."))[1] != (sys.argv[2].rsplit("."))[1]:
     sys.exit("Input and output have different extensions")
 
 
@@ -28,4 +26,4 @@ try:
         outputimage = inputfile.paste(resizedshirt, resizedshirt)
     outputimage.save(sys.argv[2])
 except FileNotFoundError:
-    sys.exit=("Invalid output or input")
+    sys.exit("Input does not exist")

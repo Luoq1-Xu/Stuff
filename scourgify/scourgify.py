3e1a820a97ac07872476008d8e6848c7150359fe
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-06-09 04:43:21 +0000

    Fri, Jun 9, 2023, 12:43 PM +08:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -7,9 +7,10 @@ if argc < 3:
 elif argc > 3:
     sys.exit("Too few command-line arguments")
 
+list = []
 try:
     with open(sys.argv[2]) as readfile:
         for line in readfile:
-            
+
 except NameError:
     sys.exit("Could not read {}".format(sys.argv[2]))

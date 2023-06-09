575939d150e90120993159aafa9928127e092967
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-06-09 05:18:34 +0000

    Fri, Jun 9, 2023, 1:18 PM +08:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -8,16 +8,20 @@ elif argc > 3:
     sys.exit("Too few command-line arguments")
 
 list = []
+counter = 0
 try:
     with open(sys.argv[1], 'r') as readfile:
         for line in readfile:
-            linelist = line.split(',')
-            for element in linelist:
-                element = element.strip('\"')
-            temp = linelist[1]
-            linelist[1] = linelist[0] + ","
-            linelist[0] = temp + ","
-            list.append(linelist)
+            if counter == 0:
+                list.append(line)
+            else:
+                linelist = line.split(',')
+                for element in linelist:
+                    element = element.strip('\"')
+                temp = linelist[1]
+                linelist[1] = linelist[0] + ","
+                linelist[0] = temp + ","
+                list.append(linelist)
     with open(sys.argv[2], 'a') as writefile:
         for line in list:
             for element in line:

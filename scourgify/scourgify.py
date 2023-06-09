8cdf8ae736ae004a846a77fe154e75fdaa8fee31
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-06-09 05:42:23 +0000

    Fri, Jun 9, 2023, 1:42 PM +08:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -13,13 +13,13 @@ try:
     with open(sys.argv[1], 'r') as readfile:
         for line in readfile:
             if counter == 0:
-                list.append(line)
+                list.append(["first,", "last,", "house\n"])
                 counter += 1
             else:
                 linelist = line.split(',')
                 temp = linelist[1]
-                linelist[1] = linelist[0] + '",'
-                linelist[0] = '"' + temp.strip() + ','
+                linelist[1] = linelist[0].strip('"') + ','
+                linelist[0] = temp.strip('" ') + ','
                 list.append(linelist)
     with open(sys.argv[2], 'a') as writefile:
         for line in list:

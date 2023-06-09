c8239fb4292c4567ec29b00bfb534851f2a592db
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-06-09 04:49:59 +0000

    Fri, Jun 9, 2023, 12:49 PM +08:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -9,10 +9,15 @@ elif argc > 3:
 
 list = []
 try:
-    with open(sys.argv[2]) as readfile:
+    with open(sys.argv[1]) as readfile:
         for line in readfile:
             linelist = line.split(',')
+            list.append(linelist)
             for element in linelist:
                 element = element.strip('"')
+    with open(sys.argv[2]) as writefile:
+        for line in list:
+            for element in line:
+                
 except NameError:
     sys.exit("Could not read {}".format(sys.argv[2]))

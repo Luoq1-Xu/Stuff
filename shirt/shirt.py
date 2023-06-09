f68e91979b33f1ce66c2adbe6f1909b199c749af
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-06-09 12:31:42 +0000

    Fri, Jun 9, 2023, 8:31 PM +08:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -7,3 +7,13 @@ if argc < 3:
     sys.exit("Too few command-line arguments")
 elif argc > 3:
     sys.exit("Too many command-line arguments")
+
+
+if ((sys.argv[1].rsplit("."))[0] not in ["jpg", "jpeg", "png"]):
+    sys.exit("Invalid input")
+if ((sys.argv[2].rsplit("."))[0] not in ["jpg", "jpeg", "png"]):
+    sys.exit("Invalid output")
+if (sys.argv[1].rsplit("."))[0] != (sys.argv[2].rsplit("."))[0]:
+    sys.exit("Input and output have different extensions")
+
+

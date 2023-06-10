4e9ab12657c29942f4f54e5732608fca87151d3b
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-06-10 03:29:23 +0000

    Sat, Jun 10, 2023, 11:29 AM +08:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -5,7 +5,7 @@ def main():
     print(validate(input("IPv4 Address: ")))
 
 def validate(ip):
-    if re.search(r"[0-255]\.[0-255]\.[0-255]\.[0-255]",ip):
+    if re.search(r"^[0-255]\.[0-255]\.[0-255]\.[0-255]$",ip):
         return "True"
     else:
         return "False"

2825655bd4d3afbd18aa71e058956b34bb4c8547
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-06-10 03:51:54 +0000

    Sat, Jun 10, 2023, 11:51 AM +08:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -5,7 +5,7 @@ def main():
     print(validate(input("IPv4 Address: ")))
 
 def validate(ip):
-    if re.search(r"^([\d{1,2}]|([0-2][0-5][0-5]))\.[(0-255)]\.[(0-255)]\.[(0-255)]$",ip):
+    if re.search(r"^([\d{1,2}]|([0-2][0-5][0-5]))\.([\d{1,2}]|([0-2][0-5][0-5]))\.([\d{1,2}]|([0-2][0-5][0-5]))\.([\d{1,2}]|([0-2][0-5][0-5]))$",ip):
         return "True"
     else:
         return "False"

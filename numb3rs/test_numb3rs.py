ef848c584f9f86c53937c125ba6a5d55bb7935af
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-06-10 04:50:52 +0000

    Sat, Jun 10, 2023, 12:50 PM +08:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -4,18 +4,9 @@ def main():
     test_validate()
 
 def test_validate():
-    try:
         assert validate("255.255.255.255") == "True"
-    except AssertionError:
-        print("Valid input was rejected")
-    try:
         assert validate("258.255.255.255") == "False"
-    except AssertionError:
-        print("Invalid input was accepted")
-    try:
         assert validate("1.1.1.1.1") == "False"
-    except AssertionError:
-        print("Invalid input was accepted")
 
 
 

9e5cad25ef3bdea14a57fd311dc0477afd349aa4
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-06-10 05:36:05 +0000

    Sat, Jun 10, 2023, 1:36 PM +08:00

--- watch/watch.py
+++ watch/watch.py
@@ -7,8 +7,9 @@ def main():
 
 
 def parse(s):
-    if match := re.search(r".*(http://www.youtube.com/embed/)", s)
+    if match := re.search(r".*(http://www.youtube.com/embed/[^\"]).*", s)
         src = match.group(1)
+        print(src)
 
 if __name__ == "__main__":
     main()

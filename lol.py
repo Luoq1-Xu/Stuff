0b838127a837daff4621f9ebc0e77679849cbce4
O-Legion <79454985+O-Legion@users.noreply.github.com>
2023-06-11 08:05:36 +0000

    Sun, Jun 11, 2023, 4:05 PM +08:00

--- lol.py
+++ lol.py
@@ -45,5 +45,8 @@ def valid_heap(node):
 
 def swap(n1, n2):
     temp = n1.value
+    templeft = n1.left
+    tempright = n2.right
     n1.value = n2.value
-    
+    n1.left = n2.left
+    n1.right = n2.right
